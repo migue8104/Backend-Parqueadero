165e9fdc10fcae14901a1755359efe3cf0d33687
migue8104 <you@example.com>
2024-08-01 16:45:08 -0500

    creacion de los entity del contexto parqueadero

---
+++ src/main/java/co/com/park/BP/entity/parqueadero/CiudadEntity.java
@@ -0,0 +1,60 @@
+package co.com.park.BP.entity.parqueadero;
+
+import java.util.UUID;
+
+import co.com.park.BP.crosscutting.helpers.ObjectHelper;
+import co.com.park.BP.crosscutting.helpers.TextHelper;
+import co.com.park.BP.crosscutting.helpers.UUIDHelper;
+
+
+
+public final class CiudadEntity {
+
+	private UUID id;
+	private String nombre;
+	private DepartamentoEntity departamento;
+
+	public CiudadEntity() {
+		setId(UUIDHelper.getDefault());
+		setNombre(TextHelper.EMPTY);
+		setDepartamento(DepartamentoEntity.build());
+	}
+
+	public CiudadEntity(final UUID id, final String nombre, final DepartamentoEntity departamento) {
+		setId(id);
+		setNombre(nombre);
+		setDepartamento(departamento);
+	}
+
+	public static final CiudadEntity build() {
+		return new CiudadEntity();
+	}
+
+	public final CiudadEntity setId(final UUID id) {
+		this.id = UUIDHelper.getDefault(id, UUIDHelper.getDefault());
+		return this;
+	}
+
+	public final CiudadEntity setNombre(final String nombre) {
+		this.nombre = TextHelper.applyTrim(nombre);
+		return this;
+	}
+
+	public final CiudadEntity setDepartamento(final DepartamentoEntity departamento) {
+		this.departamento = ObjectHelper.getObjectHelper().getDefaultValue(departamento, new DepartamentoEntity());
+		return this;
+	}
+
+	public final UUID getId() {
+		return id;
+	}
+
+	public final String getNombre() {
+		return nombre;
+	}
+
+	public final DepartamentoEntity getDepartamento() {
+		return departamento;
+	}
+
+}
